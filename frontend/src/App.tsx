1556fe54a606f9fa953fb42af2f90c9ed7280f49
marcosgalvao118 <marcos.galvao@aluno.ifsp.edu.br>
2022-07-12 12:32:04 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
-    return (
+  return (
+    <>
       <h1>Olá Mundo</h1>
-    )
+      <NotificationButton />
+      <NotificationButton />
+    </>
+  )
 }
 
 export default App
